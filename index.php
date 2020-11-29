c2c414061f4883bfc3e4b1d056b5f99080ec24a3
Ric255-r <ricardoado27@gmail.com>
2020-11-29 12:22:05 +0700

    tambah paragrap

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi corporis cumque unde eius accusantium deserunt, eos est tenetur similique iure. Amet, dolore! Nostrum ea consequatur, vero facere aperiam quam labore.</p>
 </body>
 </html>
